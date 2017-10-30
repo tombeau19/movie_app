375f198734692dcf08a3d1f02a18bd0dbaa1b79f
tombeau19 <thomas.beauregard@gmail.com>
2017-10-30 14:37:33 -0400

    created app and model for User and Movie

---
+++ db/migrate/20171030183310_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :genre
+      t.integer :year
+      t.string :synopsis
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
